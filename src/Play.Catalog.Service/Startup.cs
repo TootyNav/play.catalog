19f6c10a0ccf1f9d65bf5a327bfc3510037949e6
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2022-05-01 00:21:32 +0100

    Make mongo db readable

--- src/Play.Catalog.Service/Startup.cs
+++ src/Play.Catalog.Service/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 
 namespace Play.Catalog.Service
 {
@@ -26,13 +29,16 @@ namespace Play.Catalog.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //make mongo db guid and time look readable
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
             services.AddControllers(options =>
             {
                 //methods with async in their name are changedon run time. we need this to use nameof in controllers
-                options.SuppressAsyncSuffixInActionNames = false; 
+                options.SuppressAsyncSuffixInActionNames = false;
             });
-            
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Catalog.Service", Version = "v1" });
